2f7ec8e4c231d105c7edf37cae487adcb5889fa3
k501 <komai19850822@gmail.com>
2018-06-07 00:38:41 +0900

    aaa

--- data-survey.py
+++ data-survey.py
@@ -1,9 +1,28 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy
+%matplotlib inline
+# data frameの表示オプション
+pd.set_option('max_columns', 150)
+pd.set_option('max_rows', 150)
 
-df = pd.read_csv("./01.data/train.csv")
+# read tain data
+df = pd.read_csv('./01.data/train.csv')
+
+# 行数・列数
+df.shape
 df.head(n=7)
 
-df.tail(n=7)
+# NA survey
+
+df.isnull().sum()
+
+df.describe()
+df.iloc[:,2:10].describe()
+
+
+
+
 
 
 
